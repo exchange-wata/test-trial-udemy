77fac36841722913aaf1056a31d88f8bcc0b6932
exchange-wata <exchange.wata0907@gmail.com>
2024-01-07 11:26:33 +0900

    describeを追加

--- src/section3/mock_func.test.ts
+++ src/section3/mock_func.test.ts
@@ -1,14 +1,16 @@
-it('初めてのmock', () => {
-  const word = 'Hello Mock';
-  const mockFunc = jest.fn(() => word);
-  // console.log(mockFunc());
-  expect(mockFunc()).toBe(word);
-});
+describe('mock', () => {
+  it('初めてのmock', () => {
+    const word = 'Hello Mock';
+    const mockFunc = jest.fn(() => word);
+    // console.log(mockFunc());
+    expect(mockFunc()).toBe(word);
+  });
 
-it('mockImplementation', () => {
-  const word = 'Hello Mock mockImplementation';
-  const mockFunc = jest.fn();
-  mockFunc.mockImplementation(() => word);
-  // console.log(mockFunc());
-  expect(mockFunc()).toBe(word);
+  it('mockImplementation', () => {
+    const word = 'Hello Mock mockImplementation';
+    const mockFunc = jest.fn();
+    mockFunc.mockImplementation(() => word);
+    // console.log(mockFunc());
+    expect(mockFunc()).toBe(word);
+  });
 });
