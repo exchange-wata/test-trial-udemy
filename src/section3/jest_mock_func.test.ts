48ae9a908701cdb63f8ce07401c828a10859d7a8
exchange-wata <exchange.wata0907@gmail.com>
2024-01-07 11:08:15 +0900

    mock基本

---
+++ src/section3/jest_mock_func.test.ts
@@ -0,0 +1,14 @@
+it('初めてのmock', () => {
+  const word = 'Hello Mock';
+  const mockFunc = jest.fn(() => word);
+  // console.log(mockFunc());
+  expect(mockFunc()).toBe(word);
+});
+
+it('mockImplementation', () => {
+  const word = 'Hello Mock mockImplementation';
+  const mockFunc = jest.fn();
+  mockFunc.mockImplementation(() => word);
+  // console.log(mockFunc());
+  expect(mockFunc()).toBe(word);
+});
